6fcc676a937711643c1cd886c40cbed735c66986
andy200212 <andy200212@gmail.com>
2020-09-08 15:21:15 -0700

    fetched?

--- datarep3/array0.cc
+++ datarep3/array0.cc
@@ -3,11 +3,13 @@
 
 int main() {
     int a[4];
-
+    // homogenous collection: all elements are of the same type
+    // this is different from a struct which is a heterogenous set
     a[0] = 61;
     a[1] = 62;
     a[2] = 63;
     a[3] = 64;
 
     hexdump_object(a);
+    //note that there are symbols that correspond to each of the numbers
 }
